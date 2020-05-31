fd0107c4e4471e5b259c3904304b0f98774746a0
StephenDodd1 <60804729+StephenDodd1@users.noreply.github.com>
2020-05-31 01:35:37 -0500

    added store and imported to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import store from './store.js';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store
+    
+  }
+  render() {
+  console.log(this.state)
+  
   return (
     <div className="App">
-     
+      
     </div>
   );
+}
 }
 
 export default App;
